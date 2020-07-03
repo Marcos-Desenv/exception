3f0f0d37b9e18f07aa56586ac1f45e9d36fc57aa
Marcos-Desenv <marcos.mendes@hotmail.com.br>
2020-07-03 16:25:07 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,16 +37,11 @@ public class Program {
 			System.out.println("Check-out:");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("ERRO BEFORE");
-			} else {
-				if (!checkOut.after(checkIn)) {
-					System.out.println("NOT AFTER");
-				} else {
-					reservation.updateDates(checkIn, checkOut);
-					System.out.println("Reservation:" + reservation);
-				}
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println(error);
+			}else {
+				System.out.println("Reservation:" + reservation);
 			}
 		}
 		;
